864ac7f16198f1d0026e551a94e23e142f38465b
Yuto Ohnuki <ytohnuki@amazon.com>
2026-09-21 08:39:30 +0000

    step09: multi-vCPU with pthreads

--- microkvm.h
+++ microkvm.h
@@ -2,7 +2,11 @@
 #define MICROKVM_H
 
 /* VM configuration */
+#define NUM_VCPUS 2
 #define GUEST_MEM_SIZE (1 << 20)    /* 1 MB */
+
+/* VM address layout */
+#define VCPU1_ENTRY 0x1100          /* vCPU 1 entry point in guest binary */
 #define PIO_PORT 0x10               /* PIO port for character output */
 
 /*
